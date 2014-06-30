afdc8e320c15a9e9a5bbb402684808611c16ee8c
Maikel Hofman <mh.carducci@gmail.com>
2014-07-01 00:53:03 +0200

    Added basic Vector 3 and Vector 2 classes

---
+++ Chronos-VM-Library/Vector2.cs
@@ -0,0 +1,31 @@
+﻿namespace Chronos
+{
+    public class Vector2<T>
+    {
+        public Vector2() : this(default(T), default(T))
+        {
+
+        }
+
+        public Vector2(T x) : this(x, default(T))
+        {
+
+        }
+
+        public Vector2(T x, T y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        /// <summary>
+        /// Gets or sets the X component of the vector.
+        /// </summary>
+        public T X { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Y component of the vector.
+        /// </summary>
+        public T Y { get; set; }
+    }
+}
---
+++ Chronos-VM-Library/Vector3.cs
@@ -0,0 +1,44 @@
+﻿namespace Chronos
+{
+    public class Vector3<T>
+    {
+        public Vector3()
+            : this(default(T), default(T), default(T))
+        {
+            
+        }
+
+        public Vector3(T x)
+            : this(x, default(T), default(T))
+        {
+            
+        }
+        public Vector3(T x, T y)
+            : this(x, y, default(T))
+        {
+
+        }
+
+        public Vector3(T x, T y, T z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        /// <summary>
+        /// Gets or sets the X component of the vector.
+        /// </summary>
+        public T X { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Y component of the vector.
+        /// </summary>
+        public T Y { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Z component of the vector.
+        /// </summary>
+        public T Z { get; set; }
+    }
+}
